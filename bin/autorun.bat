7734052aadf69df15f2a09c5209c10ddd2808927
Matthew L Shields <geek@shields.cc>
2015-12-07 12:13:33 -0500

    Added bin folder and contents

---
+++ bin/autorun.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Runs when a command window is open to initiate settings
+
+REM Set aliases
+doskey /macrofile=d:\devl\bin\doskey-macros.txt
---
+++ bin/runcntlm.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\devl\software\cntlm-0.92.3\cntlm.exe -c C:\devl\software\cntlm-0.92.3\cntlm.ini -I -l 3128 -a ntlm -f
+echo Proxy listening on "http://localhost:3128"
